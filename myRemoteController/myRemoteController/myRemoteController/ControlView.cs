394759424288420acb8235bba94e5225a6d42b88
Johannes <80753737+myDevLog@users.noreply.github.com>
2021-08-16 07:32:53 +0200

    Added Cross-Platform Part

---
+++ myRemoteController/myRemoteController/myRemoteController/ControlView.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace myRemoteController
+{
+	public class ControlView{
+		protected string Name { get { return Name; } set{ Name = value; } }
+		protected string ImageName { get { return ImageName; } set { ImageName = value; } }
+
+		
+		public ControlView(string Viewname = "", string Picname = ""){
+			this.Name = Viewname;
+			this.ImageName = Picname;
+		}
+	}
+}
