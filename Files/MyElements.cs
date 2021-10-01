71fb8e5a75128b54342897dd1eff9e22d75f69f5
Johannes <80753737+myDevLog@users.noreply.github.com>
2021-10-01 17:00:53 +0200

    Refactoring - DRY-Principle to the Data Templates

---
+++ Files/MyElements.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace myRemoteController {
+	class MyElements {
+
+		private readonly MyValues Values = new MyValues();
+
+		public Label CreateLabel() {
+			Label viewLabel = new Label {
+				FontAttributes = FontAttributes.Bold,
+				FontSize = Values.GetFontSize(),
+				HorizontalTextAlignment = TextAlignment.Center,
+				Margin = Values.GetPadMarg(),
+				Padding = 0
+			};
+
+			return viewLabel;
+		}
+
+		public Image CreateImage() {
+			Image viewImage = new Image {
+				Aspect = Aspect.AspectFill
+			};
+
+			return viewImage;
+		}
+
+		public Frame CreateListFrame() {
+			Frame cellFrame = new Frame {
+				CornerRadius = Values.GetCornerRadius(),
+				IsClippedToBounds = true,
+				WidthRequest = Values.GetImgDims() / Values.GetViewDensity(),
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				Padding = 0,
+				HasShadow = false
+			};
+
+			return cellFrame;
+		}
+
+		public Frame CreateCollectionFrame() {
+			int imgDims = Values.GetImgDims();
+			
+			Frame CollectionFrame = new Frame {
+				CornerRadius = Values.GetCornerRadius(),
+				IsClippedToBounds = true,
+				WidthRequest = imgDims,
+				HeightRequest = imgDims,
+				VerticalOptions = LayoutOptions.Center,
+				HorizontalOptions = LayoutOptions.Center,
+				Padding = 0,
+				HasShadow = false
+			};
+
+			return CollectionFrame;
+		}
+	}
+}
